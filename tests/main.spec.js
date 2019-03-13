673a237387056db69e2eaaed6078cb7c78deaecc
Vitor José <vitorjsls30@gmail.com>
2019-03-13 10:46:09 -0300

    btc-01 - Adding first unit test for the CLI

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,18 @@
-import { expect } from 'chai';
+const expect = require('chai').expect; //commonjs
 
-describe('Main', () => {
+const exec = require('child_process').exec;
+const btcConverter = 'node ./src/main.js';
 
+
+describe('Main CLI', () => {
+
+    it('should return Hello World', (done) => { 
+        exec(btcConverter, (err, stdout, stderr) => {
+            if (err) {
+                throw err;
+            }
+            expect(stdout.replace('\n', '')).to.be.equal('Hello World!');
+            done();
+        });
+    });
 });
