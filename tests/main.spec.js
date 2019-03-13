bcbf9312895775b5b32d7f50a17a31910dfccd68
Vitor José <vitorjsls30@gmail.com>
2019-03-13 11:42:57 -0300

    btc-01 - Adding commander package into the project

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,16 +2,17 @@ const expect = require('chai').expect; //commonjs
 
 const exec = require('child_process').exec;
 const btcConverter = 'node ./src/main.js';
-
+const pkg = require('../package.json');
 
 describe('Main CLI', () => {
 
-    it('should return Hello World', (done) => { 
-        exec(btcConverter, (err, stdout, stderr) => {
+    it('should return version of btc-converter', (done) => { 
+        exec(`${btcConverter} --version`, (err, stdout, stderr) => {
             if (err) {
                 throw err;
             }
-            expect(stdout.replace('\n', '')).to.be.equal('Hello World!');
+            console.log(pkg.version);
+            expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
             done();
         });
     });
