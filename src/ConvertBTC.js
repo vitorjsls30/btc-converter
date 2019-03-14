6c781e29afc74fa827bafa41d209c41585b32ada
Vitor José <vitorjsls30@gmail.com>
2019-03-14 11:44:02 -0300

    btc-01 - Adding nock and request packages to communicate with the API

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -1,4 +1,21 @@
+const request = require('request');
+
 function convertBTC(currency = 'USD', amount = 1) {
-  return `${amount} BTC to ${currency} = 2000.00`;
+  const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+
+  request(url, (err, response, body) => {
+    let apiResponse;
+
+    try {
+      apiResponse = JSON.parse(body);
+    } catch (parseError) {
+      console.log('Something went wrong in the API. Try in a few minutes.');
+      return parseError;
+    }
+
+    console.log(`${amount} BTC to ${currency} = ${apiResponse.price}`);
+    return `${amount} BTC to ${currency} = ${apiResponse.price}`;
+  });
 }
+
 module.exports = convertBTC;
